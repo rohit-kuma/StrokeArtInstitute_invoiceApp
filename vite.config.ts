f08b7156137f4b75dbb722ebb94e9a65c1fdef84
Rohit Kumar <54831130+rohit-kuma@users.noreply.github.com>
2025-10-24 19:21:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+//
 // https://vitejs.dev/config/
 export default defineConfig({
   // IMPORTANT: Replace <YOUR_REPOSITORY_NAME> with the actual name of your GitHub repository.
