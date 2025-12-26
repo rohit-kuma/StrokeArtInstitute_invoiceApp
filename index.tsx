952c0396e8581599e9769826b8500166b23d177e
Rohit Kumar <rohit.k42@gmail.com>
2025-12-26 05:51:32 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,6 +4,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
+// @ts-ignore
+import { registerSW } from 'virtual:pwa-register';
+
+if ("serviceWorker" in navigator) {
+  registerSW({
+    immediate: true,
+    onRegistered(r: any) {
+      console.log('SW Registered:', r);
+    },
+    onRegisterError(error: any) {
+      console.log('SW Registration error', error);
+    },
+  });
+}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
